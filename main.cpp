714c174c0c932aa36bf9e88560dc6d2be3fe2520
Hunter-Culler <43865381+Hunter-Culler@users.noreply.github.com>
2020-11-27 23:51:31 -0700

    Added support for more functions
    Added 2 functions and formatted output

--- main.cpp
+++ main.cpp
@@ -1,103 +1,116 @@
-#include <stdio.h>
 #include <iostream>
-#include <iomanip>
-#include <omp.h>
-#include <list>
-#include <utility>
-#include <exception>
+#include <cstdio>
+#include <string>
 #include <cmath>
-#include <time.h>
-#include <stdlib.h>
+#include <omp.h>
+#include <stack>
 
 using namespace std;
 
-struct Result
-{
-    double area;
-};
+double f1(double x){
+    double result = (4 * pow(x, 6) - 2 * pow(x, 3) + (7 * x) - 4);
+    return result;
+}
 
-double f(const double x);
-const Result rectangleMethod(const double, const double, const double, const int);
-const Result trapezoidalMethod(const double, const double, const double, const int);
+double f2(double x){
+    double result = cos(x) - (3 / pow(x, 5));
+    return result;
+}
+
+double f3(double x){
+    double result = exp(x) + 1 / (pow(x, 2) + 1);
+    return result;
+}
+
+double integralF1(double a, double b){
+    double integral;
+    integral = (((4 * pow(b, 7)) / 7) - ((2 * pow(b, 4)) / 4) + ((7 * pow(b, 2)) / 2) - 4 * b)
+                    - (((4 * pow(a, 7)) / 7) - ((2 * pow(a, 4)) / 4) + ((7 * pow(a, 2)) / 2) - 4 * a);
+    return integral;
+}
+
+double integralF2(double a, double b){
+    double integral;
+    integral = (sin(b) + (3 / (4 * pow(b, 4)))) - (sin(a) + (3 / (4 * pow(a, 4))));
+    return integral;
+}
+
+double integralF3(double a, double b){
+    double integral;
+    integral = (exp(b) + atan(b)) - (exp(a) + atan(a));
+    return integral;
+}
+
+double SimpsonsRule(double a, double b, int func){
+    double integral = 0.0;
+    double c = (a + b) / 2;
+    if (func == 1){
+        integral = ((b - a) * (f1(a) + 4 * f1(c) + f1(b))) / 6;
+    }
+    else if (func == 2){
+        integral = ((b - a) * (f2(a) + 4 * f2(c) + f2(b))) / 6;
+    }
+    else if (func == 3){
+        integral = ((b - a) * (f3(a) + 4 * f3(c) + f3(b))) / 6;
+    }
+
+    return integral;
+}
+
+double getError(double val1, double val2){
+    return (abs((val1 - val2) / val1));
+}
+
+double AdaptiveQuadrature(double lower, double upper, double error, int func){
+    double simpsons = SimpsonsRule(lower, upper, func);
+    double actual;
+    if (func == 1){
+        actual = integralF1(lower, upper);
+    }
+    else if (func == 2){
+        actual = integralF2(lower, upper);
+    }
+    else if (func == 3){
+        actual = integralF3(lower, upper);
+    }
+    double integral = 0.0;
+
+    if (getError(actual, simpsons) > error){
+        double midpoint = (upper + lower) / 2;
+        integral += AdaptiveQuadrature(lower, midpoint, error, func);
+        integral += AdaptiveQuadrature(midpoint, upper, error, func);
+    }
+    else{
+        integral += simpsons;
+    }
+
+    return integral;
+}
 
 int main()
 {
-    const short maxThreads = 10;
-    short method;
-    double x1, x2, dx;
+    double approxf1, approxf2, approxf3, actualf1, actualf2, actualf3, error1, error2, error3;
+    approxf1 = AdaptiveQuadrature(0, 10, 0.02, 1);
+    approxf2 = AdaptiveQuadrature(1, 8, 0.02, 2);
+    approxf3 = AdaptiveQuadrature(0, 10, 0.02, 3);
 
-    cout << fixed << setprecision(8) << endl;
-    try
-    {
-        while (true)
-        {
-            cout << "   X1: "; cin >> x1;
-            cout << "   X2: "; cin >> x2;
-            cout << "   dx: "; cin >> dx;
-            cout << "   Method (1 - rectangle, 2 - trapezoidal): "; cin >> method;
+    actualf1 = integralF1(0, 10);
+    actualf2 = integralF2(1, 8);
+    actualf3 = integralF3(0, 10);
 
-            list<pair<short, Result> > results;
-            for (int i = 0; i < maxThreads; i++)
-            {
-                Result result = (method == 1) ?
-                                rectangleMethod(x1, x2, dx, i + 1) :
-                                trapezoidalMethod(x1, x2, dx, i + 1);
+    error1 = getError(actualf1, approxf1);
+    error2 = getError(actualf2, approxf2);
+    error3 = getError(actualf3, approxf3);
 
-                pair<short, Result> s_result(i + 1, result);
-                results.push_back(s_result);
-            }
+    cout << "Approximate integral of f(x) = 4x^6 - 2x^3 + 7x - 4 from 0 to 10: " << approxf1 << endl;
+    cout << "Actual integral of f(x) = 4x^6 - 2x^3 + 7x - 4 from 0 to 10: " << actualf1 << endl;
+    cout << "Error: " << error1 << "\n\n";
+    cout << "Approximated f(x) = cos(x) - 3x^-5 from 1 to 8: " << approxf2 << endl;
+    cout << "Actual f(x) = cos(x) - 3x^-5 from 1 to 8: " << actualf2 << endl;
+    cout << "Error: " << error2 << "\n\n";
+    cout << "Approximated f(x) = e^x + 1 / (x^2 + 1) from 0 to 10: " << approxf3 << endl;
+    cout << "Actual f(x) = e^x + 1 / (x^2 + 1) from 0 to 10: " << actualf3 << endl;
+    cout << "Error: " << error3 << "\n\n";
 
-            cout << endl << "   Results:" << endl;
-            for (int i = 0; i < results.size(); i++)
-            {
-                cout << "   Threads: " << results.front().first;
-                cout << ", area: " << results.front().second.area << endl;
-                results.pop_front();
-            }
-            cout << endl;
-        }
-    }
-    catch (exception & e)
-    {
-        cout << e.what() << endl;
-    }
-    cin.get();
     return 0;
-}
-
-const Result rectangleMethod(const double x1, const double x2, const double dx, const int nThreads)
-{
-    const int N = static_cast<int>((x2 - x1) / dx);
-    double s = 0;
-
-    #pragma omp parallel for num_threads(nThreads) reduction(+: s)
-    for (int i = 1; i <= N; i++) s += f(x1 + i * dx);
-
-    s *= dx;
-
-    Result temp;
-    temp.area = s;
-
-    return temp;
-}
-
-const Result trapezoidalMethod(const double x1, const double x2, const double dx, const int nThreads)
-{
-    const int N = static_cast<int>((x2 - x1) / dx);
-    double s = 0;
-
-    #pragma omp parallel for num_threads(nThreads) reduction(+: s)
-    for (int i = 1; i < N; i++) s += f(x1 + i * dx);
-
-    s = (s + (f(x1) + f(x2)) / 2) * dx;
-
-    Result temp;
-    temp.area = s;
-
-    return temp;
-}
-
-double f(const double x)
-{
-    return sin(x);
 }
